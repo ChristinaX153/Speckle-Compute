cf0da0443ae04224455fe0b833aecd492d2fdba2
ChristinaX153 <christina.christoforou@students.iaac.net>
2025-03-23 21:37:44 +0200

    Finally everything works

--- fetch.py
+++ fetch.py
@@ -8,7 +8,7 @@ def send_to_compute():
     compute_rhino3d.Util.url = "http://3.78.144.62/"
     compute_rhino3d.Util.apiKey = "SpeckleCompute02"
     send_to_compute_stream = "https://macad.speckle.xyz/projects/40656cdf62/models/1abbc8b816"
-    receive_from_compute_stream = "https://macad.speckle.xyz/projects/ee848193f6"
+    receive_from_compute_stream = "https://macad.speckle.xyz/projects/ee848193f6/models/4ea327960c"
     gh_definition = "120325_Hyper a_collab.ghx"
 
     gh_data = open(gh_definition, mode="r", encoding="utf-8-sig").read()
